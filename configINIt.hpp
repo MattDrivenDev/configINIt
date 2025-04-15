45176f43a932b0ff8305acb445bc91c62379bc5d
Matt Ball <mattdrivendev@gmail.com>
2025-04-15 14:56:11 +0100

    Minor fixes/changes to tidy up.

--- configINIt.hpp
+++ configINIt.hpp
@@ -6,99 +6,103 @@
 class configINIt {
 private:
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> data;
-    inline void TrimString(std::string& s);
-    inline bool IsCommentLine(std::string& line);
-    inline bool IsSectionLine(std::string& line);
-    inline bool IsKeyValueLine(const std::string& line, size_t& pos);
-public: 
-    bool Load(const std::string& filename);
-    std::string GetString(const std::string& section, const std::string& key, const std::string& default) const;
-};
 
-bool configINIt::Load(const std::string& filename)
-{
-    std::ifstream file(filename);
-    if (!file.is_open())
+    inline void TrimString(std::string& s)
     {
-        return false;
+        if (s.empty())
+        {
+            return;
+        }
+
+        const char* whitespace= " \t\r\n";
+        size_t start = s.find_first_not_of(whitespace);
+        if (start == std::string::npos)
+        {
+            return;
+        }
+        size_t end = s.find_last_not_of(whitespace);
+        s = s.substr(start, end - start + 1);
     }
 
-    std::string line, section;
-
-    while (std::getline(file, line))
+    inline bool IsCommentLine(const std::string& line)
     {
-        TrimString(line);
-        
-        if (line.empty() || IsCommentLine(line))
+        return !line.empty() && (line[0] == ';') || (line[0] == '#');
+    }
+
+    inline bool IsSectionLine(const std::string& line)
+    {
+        return !line.empty() && (line[0] == '[') && (line.back() == ']');
+    }
+
+    inline bool IsKeyValueLine(const std::string& line, size_t& pos)
+    {
+        if (line.empty())
         {
-            continue;
+            return false;
         }
 
-        if (IsSectionLine(line))
-        {
-            section = line.substr(1, line.length() - 2);
-            TrimString(section);
-        }
-        else
-        {
-            size_t pos;
-            std::string key, value;
+        pos = line.find('=');
+        return pos != std::string::npos;
+    }
 
-            if (!IsKeyValueLine(line, pos))
+public: 
+    bool Load(const std::string& filename)
+    {
+        std::ifstream file(filename);
+        if (!file.is_open())
+        {
+            return false;
+        }
+    
+        std::string line, section;
+    
+        while (std::getline(file, line))
+        {
+            TrimString(line);
+            
+            if (line.empty() || IsCommentLine(line))
             {
                 continue;
             }
-
-            key = line.substr(0, pos);
-            value = line.substr(pos + 1);
-            TrimString(key);
-            TrimString(value);
-            data[section][key] = value;
+    
+            if (IsSectionLine(line))
+            {
+                section = line.substr(1, line.length() - 2);
+                TrimString(section);
+            }
+            else
+            {
+                size_t pos;
+                std::string key, value;
+    
+                if (!IsKeyValueLine(line, pos))
+                {
+                    continue;
+                }
+    
+                key = line.substr(0, pos);
+                value = line.substr(pos + 1);
+                TrimString(key);
+                TrimString(value);
+                data[section][key] = value;
+            }
         }
+    
+        return true;
     }
 
-    return true;
-}
-
-std::string configINIt::GetString(const std::string& sectionKey, const std::string& key, const std::string& defaultValue) const
-{
-    auto section = data.find(sectionKey);
-    if (section != data.end())
+    std::string GetString(const std::string& sectionKey, const std::string& key, const std::string& defaultValue) const
     {
-        auto kvp = section->second.find(key);
-        if (kvp != section->second.end())
+        auto section = data.find(sectionKey);
+        if (section != data.end())
         {
-            return kvp->second;
+            auto kvp = section->second.find(key);
+            if (kvp != section->second.end())
+            {
+                return kvp->second;
+            }
         }
+    
+        return defaultValue;
     }
-
-    return defaultValue;
-}
-
-inline void configINIt::TrimString(std::string& s)
-{
-    const char* whitespace= " \t\r\n";
-    size_t start = s.find_first_not_of(whitespace);
-    if (start == std::string::npos)
-    {
-        return;
-    }
-    size_t end = s.find_last_not_of(whitespace);
-    s = s.substr(start, end - start + 1);
-}
-
-inline bool configINIt::IsCommentLine(std::string& line)
-{
-    return (line[0] == ';') || (line[0] == '#');
-}
-
-inline bool configINIt::IsSectionLine(std::string& line)
-{
-    return (line[0] == '[') && (line.back() == ']');
-}
-
-inline bool configINIt::IsKeyValueLine(const std::string& line, size_t& pos)
-{
-    pos = line.find('=');
-    return pos != std::string::npos;
-}
+};
--- test/main.cpp
+++ test/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    std::string section1Key1, section1Key2;
+    std::string section1Key1, section1Key2, section2Key1, section2Key2;
     configINIt config;
 
     std::cout << "Loading config... ";
@@ -12,9 +12,13 @@ int main()
 
     std::cout << "Done!\r\n";
 
-    section1Key1 = config.GetString("section1", "key1", "section one key1 failed to read");
-    section1Key2 = config.GetString("section1", "key2", "section one key2 failed to read");
+    section1Key1 = config.GetString("section1", "key1", "section1.key1 failed to read");
+    section1Key2 = config.GetString("section1", "key2", "section1.key2 failed to read");
+    section2Key1 = config.GetString("section2", "key1", "section2.key1 failed to read");
+    section2Key2 = config.GetString("section2", "key2", "section2.key2 failed to read");
 
     std::cout << "section1.key1: " << section1Key1 << "\r\n";
     std::cout << "section1.key2: " << section1Key2 << "\r\n";
+    std::cout << "section2.key1: " << section2Key1 << "\r\n";
+    std::cout << "section2.key2: " << section2Key2 << "\r\n";
 }

5bfdc1c79c9fd730f8d5c7da0b1d49afb3b819d5
Matt Ball <mattdrivendev@gmail.com>
2025-04-16 14:33:13 +0100

    Lunchtime-funtime: a little environment fiddling to enable debugging in vscode on windows

--- test/main.cpp
+++ test/main.cpp
@@ -1,24 +1,22 @@
 #include "../configINIt.hpp"
+#include <filesystem>
 #include <iostream>
 
 int main()
 {
-    std::string section1Key1, section1Key2, section2Key1, section2Key2;
     configINIt config;
 
-    std::cout << "Loading config... ";
-
-    config.Load("config.ini");
+    std::cout << "Loading config... ";    
+    if (!config.Load("config.ini"))
+    {
+        std::cout << "Failed!\r\n";
+        throw 1;
+    }
 
     std::cout << "Done!\r\n";
-
-    section1Key1 = config.GetString("section1", "key1", "section1.key1 failed to read");
-    section1Key2 = config.GetString("section1", "key2", "section1.key2 failed to read");
-    section2Key1 = config.GetString("section2", "key1", "section2.key1 failed to read");
-    section2Key2 = config.GetString("section2", "key2", "section2.key2 failed to read");
-
-    std::cout << "section1.key1: " << section1Key1 << "\r\n";
-    std::cout << "section1.key2: " << section1Key2 << "\r\n";
-    std::cout << "section2.key1: " << section2Key1 << "\r\n";
-    std::cout << "section2.key2: " << section2Key2 << "\r\n";
+    
+    std::cout << "section1.key1: " << config.GetString("section1", "key1", "section1.key1 failed to read") << "\r\n";
+    std::cout << "section1.key2: " << config.GetString("section1", "key2", "section1.key2 failed to read") << "\r\n";
+    std::cout << "section2.key1: " << config.GetString("section2", "key1", "section2.key1 failed to read") << "\r\n";
+    std::cout << "section2.key2: " << config.GetString("section2", "key2", "section2.key2 failed to read") << "\r\n";
 }
